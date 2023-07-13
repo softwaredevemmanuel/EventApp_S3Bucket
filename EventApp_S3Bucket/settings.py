0e6c1788aed4fc3c52850cb3596e676e98dee808
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-13 14:30:27 +0100

    zeet database

--- EventApp_S3Bucket/settings.py
+++ EventApp_S3Bucket/settings.py
@@ -65,23 +65,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'EventApp_S3Bucket.wsgi.application'
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'hannah',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'hannah',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
