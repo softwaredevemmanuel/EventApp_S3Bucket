f3fe60b61d722e3737b387280c16de40a9538f64
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-14 20:18:08 +0100

    set

--- EventApp_S3Bucket/settings.py
+++ EventApp_S3Bucket/settings.py
@@ -72,16 +72,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event',
-        'USER': 'postgres',
-        'PASSWORD': 'hannah',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event',
+#         'USER': 'postgres',
+#         'PASSWORD': 'hannah',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
