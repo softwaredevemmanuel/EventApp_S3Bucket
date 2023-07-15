5ba38da3406f16d7f73d3dd8155a35924aecec11
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-15 09:48:11 +0100

    basedir

--- EventApp_S3Bucket/settings.py
+++ EventApp_S3Bucket/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'EventApp_S3Bucket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 
 # Default primary key field type
