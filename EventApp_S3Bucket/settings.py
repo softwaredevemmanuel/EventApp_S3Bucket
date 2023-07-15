6568d88a93193a18cae3a42bcfd6b6639cbd5bbc
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-15 09:40:45 +0100

    media

--- EventApp_S3Bucket/settings.py
+++ EventApp_S3Bucket/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 
 # Default primary key field type
