64323d2d0d48f3ad81691ea86172078d66aa47f1
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-07-16 20:10:58 +0100

    remove build static

--- build.sh
+++ build.sh
@@ -9,7 +9,3 @@ echo "Make Migration......."
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-echo "Collect Static......."
-
-python3.9 manage.py collectstatic --noinput --clear
